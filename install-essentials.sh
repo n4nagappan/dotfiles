3adad6117ec0aac77526aa4b8754dc0469e93472
Nagappan <n4nagappan@gmail.com>
2018-09-17 10:03:09 +0000

    Install nvm

--- install-essentials.sh
+++ install-essentials.sh
@@ -12,6 +12,5 @@ sudo apt-get --assume-yes install cmake \
 		# autojump
 
 
-sudo apt-add-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install --assume-yes neovim
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.nvm/nvm.sh && nvm install stable && nvm alias defautl stable
